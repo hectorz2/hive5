0483b956d43a97a5fd21208a9d9b99665d83a82e
Héctor Zaragoza Arranz <hector.zaragoza@arold.es>
2020-01-24 15:03:31 +0100

    Added switches styles. Upgraded JQuery. Fixed plugin error of JQuery upgrade. Fixed bug of header.

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Author: Héctor Zaragoza Arranz
+ * Date: 24/01/2020
+ */
+$(document).ready(function() {
+    checkInitialHeading();
+});
+
+/**
+ * This function is a copy of scroll event of template/js/main.js because if you refresh, the browser keeps the scroll
+ * position and the code is not executed, so the header is hidden until user scrolls, if we execute it on document ready
+ * it is fixed.
+ */
+function checkInitialHeading() {
+    let scroll = $(window).scrollTop();
+    if (scroll < 400) {
+        $('#sticky-header').removeClass('sticky');
+        $('#back-top').fadeIn(500);
+    } else {
+        $('#sticky-header').addClass('sticky');
+        $('#back-top').fadeIn(500);
+    }
+}
